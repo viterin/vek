850406147e4968b56e39c95c9c8c926ac7d6a502
olivier <olivier@viterin.com>
2022-09-25 21:21:37 +0200

    Disable macos acceleration by default for now

--- internal/functions/accel_amd64.go
+++ internal/functions/accel_amd64.go
@@ -1,5 +1,8 @@
 package functions
 
-import "golang.org/x/sys/cpu"
+import (
+	"golang.org/x/sys/cpu"
+	"runtime"
+)
 
-var UseAVX2 bool = cpu.X86.HasAVX2 && cpu.X86.HasFMA
+var UseAVX2 bool = cpu.X86.HasAVX2 && cpu.X86.HasFMA && runtime.GOOS != "darwin"
